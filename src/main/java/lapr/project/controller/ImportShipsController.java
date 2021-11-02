d8a7a80f0f401136a75bf30a2d39f1953503baf9
DESKTOP-LPMIHQM\Guilherme Cunha <1201506@isep.ipp.pt>
2021-11-02 14:45:48 +0000

    Add ShipDTO [Implementation] [1201506]

--- src/main/java/lapr/project/controller/ImportShipsController.java
+++ src/main/java/lapr/project/controller/ImportShipsController.java
@@ -2,12 +2,13 @@ package lapr.project.controller;
 
 import lapr.project.model.Ship;
 import lapr.project.utils.BST;
+import lapr.project.utils.ShipDTO;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class LoadFile {
+public class ImportShipsController {
 
     public static BST<Ship> importShips(String file) throws FileNotFoundException {
         BST<Ship> bst = new BST<>();
@@ -18,7 +19,8 @@ public class LoadFile {
 
         while(scan.hasNextLine()){
             String buff[] = scan.nextLine().trim().split(",");
-            Ship ship = new Ship(Integer.parseInt(buff[0]),buff[1],Double.parseDouble(buff[2]),Double.parseDouble(buff[3]), Float.parseFloat(buff[4]),Float.parseFloat(buff[5]),Integer.parseInt(buff[6]),buff[7],buff[8],buff[9],Integer.parseInt(buff[10]),Integer.parseInt(buff[11]),Integer.parseInt(buff[12]),Double.parseDouble(buff[13]),buff[14],buff[15]);
+            ShipDTO shipDTO=new ShipDTO(Integer.parseInt(buff[0]),buff[1],Double.parseDouble(buff[2]),Double.parseDouble(buff[3]), Float.parseFloat(buff[4]),Float.parseFloat(buff[5]),Integer.parseInt(buff[6]),buff[7],buff[8],buff[9],Integer.parseInt(buff[10]),Integer.parseInt(buff[11]),Integer.parseInt(buff[12]),Double.parseDouble(buff[13]),buff[14],buff[15]);
+            Ship ship = new Ship(shipDTO);
             bst.insert(ship);
         }
         return bst;
