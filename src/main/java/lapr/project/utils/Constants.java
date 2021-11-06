2579adfd38faf7261983fb1f071855f1c5c0063d
DESKTOP-LPMIHQM\Guilherme Cunha <1201506@isep.ipp.pt>
2021-11-06 13:08:41 +0000

    G111-2 #time 30min #comment PConstants class [Implementation]

---
+++ src/main/java/lapr/project/utils/Constants.java
@@ -0,0 +1,17 @@
+package lapr.project.utils;
+
+public class Constants {
+
+    //ship
+    public static final double POWER_OUTPUT = 20;
+    public static final int CALL_SIGN_DIGITS = 5;
+    public static final int MMSI_DIGITS = 9;
+    public static final double COG_HEAD_MIN = 0;
+    public static final double COG_HEAD_MAX = 359;
+    public static final String NOT_AVAILABLE = "not available";
+    //haversine
+    public static final double earthRadius=6371;
+    public static final int meters=1000;
+    public static final int division=2;
+
+}
